194f375ea1bf5b8076759cde977774b5d5b26ff6
moon-start <login0516mp4@gmail.com>
2020-02-07 01:39:09 +0800

    All File

--- database/migrations/2020_02_06_172924_create_qqkk_table.php
+++ database/migrations/2020_02_06_172924_create_qqkk_table.php
@@ -13,17 +13,9 @@ class CreateQqkkTable extends Migration
      */
     public function up()
     {
- 
-        Schema::create('qqkk', function (Blueprint $table)
-        {
-        //$table->bigIncrements('id'); 這欄位會形成 Primary Key
-        $table->bigIncrements('id');
-        $table->char('Cusid',100)->unique()->index();
-        $table->char('Name',50)->index();
-        $table->char('Address',255);
-        $table->char('Phone',10)->index();
-        //協助建立追踪的時間
-        $table->timestamps();
+        Schema::create('qqkk', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
         });
     }
 
