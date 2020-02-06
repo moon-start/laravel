58fc51ca068bf3920f427f71343db1bc19a6625b
moon-start <login0516mp4@gmail.com>
2020-02-07 02:40:04 +0800

    All File

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -19,10 +19,7 @@
                 <td>A001</td>
                 <td>王小明</td>
                 <td>0912345678</td>
-                <!-- <td>
-                  <a href="{{ route('customers',['Cusid' => $Cusid -> customer -> Cusid]) }}" class="btn btn-info btn-sm">查看</a>
-                  <a href="{{ action('SchoolController@edit', ['Cusid'=> $Cusid-> customer -> Cusid]) }}" class="btn btn-success btn-sm">編輯</a>
-                </td> -->
+                
               </tr>
             </tbody>
           </table>
