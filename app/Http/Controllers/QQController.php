9ea48e03e8fdedb166de3e53c0599400d0dd8cff
moon-start <login0516mp4@gmail.com>
2020-02-07 13:17:26 +0800

    All File

--- app/Http/Controllers/QQController.php
+++ app/Http/Controllers/QQController.php
@@ -7,7 +7,8 @@ namespace App\Http\Controllers;
 ## 建議是一個 資料表名稱
 use App\QQ;
 
-use Illuminate\Database\QueryException;
+## 多了這行? 這好像是語法自動新增..
+## use Illuminate\Database\QueryException;
 
 use Illuminate\Http\Request;
 ## 新增兩行 (一-二-三行 都用到)
