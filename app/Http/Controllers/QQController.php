c61b657872052d13f9fded68d60f4d89740835a5
moon-start <login0516mp4@gmail.com>
2020-02-10 18:31:42 +0800

    All File

--- app/Http/Controllers/QQController.php
+++ app/Http/Controllers/QQController.php
@@ -38,7 +38,7 @@ class QQController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($GG)
     {
         ## 專案/resources/views (等於) View::make
 
