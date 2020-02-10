9f61e3f6f09b5d3786ce13668ba45814838a53a9
moon-start <login0516mp4@gmail.com>
2020-02-10 16:52:39 +0800

    All File

--- resources/views/boardB.blade.php
+++ resources/views/boardB.blade.php
@@ -21,7 +21,7 @@
               <td>王小明</td>
               <td>0912345678</td>
               <td>
-              Hello, {{{ $name }}}.
+              Hello, {{ $name }}.
               </td>
             </tr>
             </tbody>  
