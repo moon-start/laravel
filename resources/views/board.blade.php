961f4c579d067bb8db765e49a960855f51c8e4e5
moon-start <login0516mp4@gmail.com>
2020-02-10 21:14:55 +0800

    All File

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -15,17 +15,7 @@
               </tr>
             </thead>
 
-            <tbody>
-            <tr>
-              <td>A001</td>
-              <td>王小明</td>
-              <td>0912345678</td>
-              <td>
-                <a href="{{ action('QQController@edit', ['Cusid'=> $Cusid-> customer -> Cusid]) }}" class="btn btn-success btn-sm">編輯</a>
-    
-              </td>
-            </tr>
-            </tbody>  
+           
          
 
             <tbody>
