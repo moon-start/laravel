ccf6c0d9a762065c8242e35379be1bf9c94bc236
moon-start <login0516mp4@gmail.com>
2020-02-10 23:14:28 +0800

    All File

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -21,7 +21,8 @@
               <td>王小明</td>
               <td>0912345678</td>
               <td>
-              
+                <a href="{{ action('edit', ['Cusid'=> '1']) }}" class="btn btn-success btn-sm">編輯</a>
+    
               </td>
             </tr>
             </tbody>  
