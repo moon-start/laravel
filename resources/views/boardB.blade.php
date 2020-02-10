fc42c1e20e105a9529cc70dc6d443c5b9b0b0706
moon-start <login0516mp4@gmail.com>
2020-02-10 22:25:19 +0800

    All File

--- resources/views/boardB.blade.php
+++ resources/views/boardB.blade.php
@@ -25,7 +25,7 @@
 
               http://192.168.1.114/QQ = {{action('QQController@index')}}  .<br>
 
-              {{ action('QQController@edit', ['Cusid'=> '1']) }}  .<br>
+              {{ action('QQController@edit' }}  .<br>
 
               
               </td>
