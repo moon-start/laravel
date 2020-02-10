a1d66b69692aca8d7b164fb36602c3238f3bf2fd
moon-start <login0516mp4@gmail.com>
2020-02-10 18:26:23 +0800

    All File

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 ## 執行 QQ控制器  的index() 
 ## @index  錯誤??
-Route::resource('QQ/{id}/{GG}','QQController');
+Route::resource('QQ/{GG}/products','QQController');
 Route::get('edit/{Cusid}','QQController@edit')->name('edit');
 
 
