063575d8be744fae34917d15479721947728baad
moon-start <login0516mp4@gmail.com>
2020-02-11 16:24:53 +0800

    All File

--- app/Http/Controllers/QQController.php
+++ app/Http/Controllers/QQController.php
@@ -175,7 +175,7 @@ class QQController extends Controller
 
     ## Route::get('edit/{Cusid}','QQController@edit')->name('edit');
     ## 表示 $Cusid,
-    public function edit(EditCustomer $request)
+    public function edit(App\Http\Requests\EditCustomer $request)
     {
         // $validator = Validator::make(
         //     $request->all(),[
