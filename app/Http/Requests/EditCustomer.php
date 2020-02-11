285974d964bf34ab67b9e44150ff40db48463f3e
moon-start <login0516mp4@gmail.com>
2020-02-11 15:12:38 +0800

    All File

--- app/Http/Requests/EditCustomer.php
+++ app/Http/Requests/EditCustomer.php
@@ -39,4 +39,25 @@ class EditCustomer extends FormRequest
         'string' => '必須為字串'
         ];
     }
+
+    // public function messages()
+    // {
+    //     return [
+    //         "userId.required" => "使用者 ID 為避填資料",
+    //         "userId.exists" => '使用者 ID 必須存在於資料庫中',
+    //         "postId.integer" => "文章 ID 必須為數值",
+    //         "postId.exists" => "文章 ID 不存在"
+    //     ];
+    // }
+
+    protected function failedValidation(Validator $validator)
+    {
+    }
+    // 不做任何動作，由 controller 處理: 需要將 getValidatorInstance() 改為 public，
+    // 讓 controller 可以取到 validator 實例並進一步得到錯誤訊息
+    public function getValidatorInstance()
+    {
+        return parent::getValidatorInstance();
+    }
 }
+
