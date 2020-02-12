1aaa2eed7334423bea3e3444554ac04b3798a5a6
moon-start <login0516mp4@gmail.com>
2020-02-13 01:51:31 +0800

    All File

--- app/Http/Controllers/QQController.php
+++ app/Http/Controllers/QQController.php
@@ -123,7 +123,9 @@ class QQController extends Controller
 
         ### B
         // $customer = Payment::find(1);
-        $customer = Payment::find(1)->invoice->Shop_name;
+        // $customer = Payment::find(1)->invoice->Shop_name;
+        $customer = Payment::find(1)->invoice;
+
         //get 付款方式...............->外部資料表->欄位
         // $customer = Payment::find(1)->invoiceppp->Shop_name;
         // $customer = Payment::find(1)->invoiceppp->find(1);;
