e2593d46be923d2f15638870429ee4001fe78491
moon-start <login0516mp4@gmail.com>
2020-02-13 00:56:45 +0800

    All File

--- database/migrations/2020_02_12_100348_create_paymentppp_table.php
+++ database/migrations/2020_02_12_100348_create_paymentppp_table.php
@@ -14,7 +14,8 @@ class CreatePaymentpppTable extends Migration
     public function up()
     {
         Schema::create('paymentppp', function (Blueprint $table) {
-            $table->increments('Payment_id');  //int 主見
+            // $table->increments('Payment_id');  //int 主見
+            $table->increments('id');  //int 主見
             $table->string('Invoice_ID',20)->index();
             $table->string('Payment_name',20)->index();
             $table->integer('Payment_price')->index();
