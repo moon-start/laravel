f0cb0e1f94c8d2871b8dfabf440eae0aec204851
moon-start <login0516mp4@gmail.com>
2020-02-13 20:02:38 +0800

    All File

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 
 ## 執行 QQ控制器  的index() 
 ## @index  錯誤??
-Route::resource('QQ','QQController')->name('QQ');;
+Route::resource('QQ','QQController');
 
 // ## 驗證
 // Route::get('post/create', 'QQController@create');
