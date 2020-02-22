0d39f32a82ec3fb7fb69369a37498340963fcf07
moon-start <login0516mp4@gmail.com>
2020-02-23 03:19:59 +0800

    All File

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,14 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-light navbar-default">
-  <div class="container">
-    <a href="{{ url('/') }}" class="navbar-brand">
-      HelloWorld
-    </a>
-    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a href="{{ action('QQController@index') }}" class="nav-link">
-          客戶列表
+    <div class="container">
+        <a href="{{ url('/') }}" class="navbar-brand">
+        專案站台
         </a>
-      </li>
-    </ul>
-  </div>
+        <ul class="navbar-nav ml-auto mt-4 mt-lg-0">
+            <li class="nav-item active">  
+                <a href="{{ action('QQController@index') }}" class="nav-link">
+                客戶列表
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ action('QQController@new') }}" class="nav-link">
+                新增客戶
+                </a>
+            </li>
+        </ul>
+    </div>
 </nav>
