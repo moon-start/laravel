4c009691cb3bb27193613dc7a45e875014cbeada
moon-start <login0516mp4@gmail.com>
2020-02-22 18:15:08 +0800

    All File

--- app/Http/Controllers/QQController.php
+++ app/Http/Controllers/QQController.php
@@ -265,8 +265,8 @@ class QQController extends Controller
 
      //刪除客戶資料
      public function delete(Request $request){
-      Customer::where('Cusid',$request->input('Cusid'))->delete();
-      $customers = Customer::all();
+      QQ::where('Cusid',$request->input('Cusid'))->delete();
+      $customers = QQ::all();
       return View::make('board',['customers' => $customers]); 
   }
 }
