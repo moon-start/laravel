bbdf3567fb55cefd1234ac9386ee1535eed5145e
moon-start <login0516mp4@gmail.com>
2020-02-23 16:55:31 +0800

    All File

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,52 +1,54 @@
 @extends('layouts.master')
+@section('title','編輯客戶資料')
 @section('content')
-<div class="row justify-content-center">
-  <div class="col-md-10">
-    <div class="card">
-      <div class="card-header">
-        <h4 class="m-0">編輯客戶資料</h4>
-      </div>
-      <div class="card-body">
-          @isset($msg)
-          <div class="alert alert-success" role="alert">
-            {{ $msg ?? '沒有任何訊息' }}
-            <button type="button" class="close" data-dismiss="alert" aria-label="CLose">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          @endisset
-
-          <form action="{{ action('QQController@update', ['Cusid' => $Cusid]) }}" method="POST">
-            {{ csrf_field() }}
-            <div class="form-group">
-              <label for="name">客戶姓名</label>
-              <input type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" name="name" value="{{ $customer->Name }}">
-                 @if ($errors->has('name'))
-                   <div class="invalid-feeback">
-                    <strong>{{ $errors->first('name') }}</strong>
-                   </div>
-                 @endif
+<form action="{{ action('QQController@update') }}" method="post">
+    @csrf
+    <div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+        <div class="card-header">編輯客戶資料</div>
+        <div class="card-body p-1">
+            <div class="form-group row">
+                <div class="col-sm-2"></div>
+                <label for="Cusid" class="col-sm-2 col-form-label">客戶編號</label>
+                <div class="col-sm-8">
+                    <input type="text"class="form-control" id="Cusid" name="Cusid" value="<?php echo $_GET['Cusid']; ?>">
+                </div>
             </div>
-            <div class="form-group">
-              <label for="phone">客戶電話</label>
-              <input type="text" class="form-control {{ $errors->has('phone') ? 'is-invalid' : '' }}" name="phone" value="{{ $customer->Phone }}">
-                 @if ($errors->has('phone'))
-                   <div class="invalid-feeback">
-                    <strong>{{ $errors->first('phone') }}</strong>
-                   </div>
-                 @endif
+            <div class="form-group row">
+                <div class="col-sm-2"></div>
+                <label for="Name" class="col-sm-2 col-form-label">客戶姓名</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" id="Name" name="Name" value="<?php echo $_GET['Name']; ?>">
+                </div>
             </div>
-            <div class="row">
-              <div class="col-md-6 mb-1">
-                <button type="submit" class="btn btn-primary btn-block">修改</button>
-              </div>
-              <div class="col-md-6">
-                <a href="{{ url('/board') }}" class="btn btn-danger btn-block">返回</a>
-              </div>
+            <div class="form-group row">
+                <div class="col-sm-2"></div>
+                <label for="Address" class="col-sm-2 col-form-label">通訊地址</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" id="Address" name="Address" value="<?php echo $_GET['Address']; ?>">  
+                </div>
             </div>
-          </form>
-        </div>  
-      </div>
+            <div class="form-group row">
+                <div class="col-sm-2"></div>
+                <label for="Phone" class="col-sm-2 col-form-label">連絡電話</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" id="Phone" name="Phone" value="<?php echo $_GET['Phone']; ?>">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-1"></div>
+                <div class="col-sm-9">
+                    <input type="submit" class="btn btn-primary" value="送出">
+                    <input type="hidden" id="oldId" name="oldId"  value="<?php echo $_GET['Cusid']; ?>">
+                </div>
+                <div class="col-sm-1">
+                    <input type="submit" class="btn btn-warning" value="取消" name="cancel">
+                </div>
+            </div>  
+        </div>
+        </div>
     </div>
-  </div>
-@endsection
+    </div>
+</form>
+@stop
