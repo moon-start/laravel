0cb9bf80881c361272e8d11ebe8c8a3c8c4ebea2
moon-start <login0516mp4@gmail.com>
2020-02-27 02:00:28 +0800

    All File

--- app/Http/Controllers/QBController.php
+++ app/Http/Controllers/QBController.php
@@ -43,7 +43,7 @@ class QBController extends Controller
         $customers = Invoice::all();  
         
         // $points = Invoice::find(1)->PaymentOf->points;
-        $points = Invoice::find(1)->Item;
+        $points = Invoice::find(1)->Item->item;
 
 
         return View::make('board',['customers' => $customers]);
