922fbf40680f8277b0c5c423830859180e45e758
moon-start <login0516mp4@gmail.com>
2020-02-28 00:44:57 +0800

    All File

--- app/Payment.php
+++ app/Payment.php
@@ -33,24 +33,24 @@ class Payment extends Model
     //     'date'
     // ];
 
-    public function invoiceOf()
-    {
-        ### 和 Payment 做一對一關聯
-        ### 一對一的連接欄位
-        // return $this->hasOne('App\Invoice','Payment_id','Payment_ID');
-        // return $this->hasOne('App\Invoice','id','Payment_ID');
+    // public function invoiceOf()
+    // {
+    //     ### 和 Payment 做一對一關聯
+    //     ### 一對一的連接欄位
+    //     // return $this->hasOne('App\Invoice','Payment_id','Payment_ID');
+    //     // return $this->hasOne('App\Invoice','id','Payment_ID');
 
 
-        ######  Invoice ~Payment_ID  對  Payment ~id
-        return $this->hasOne('App\Invoice','Payment_ID');
-        // return $this->hasOne(Invoice::class,'Payment_ID');
+    //     ######  Invoice ~Payment_ID  對  Payment ~id
+    //     return $this->hasOne('App\Invoice','Payment_ID');
+    //     // return $this->hasOne(Invoice::class,'Payment_ID');
 
         
-        ##  使用方式
-        //   //利用下列程式碼呼叫對應的客戶資料
-        //   $customer = App\Payment::find(1)->invoice;
-        //   //取得客戶電話
-        //   $customer = App\Payment::find(1)->invoice->隨便一個欄位;
+    //     ##  使用方式
+    //     //   //利用下列程式碼呼叫對應的客戶資料
+    //     //   $customer = App\Payment::find(1)->invoice;
+    //     //   //取得客戶電話
+    //     //   $customer = App\Payment::find(1)->invoice->隨便一個欄位;
   
-    }
+    // }
 }
