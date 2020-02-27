071247ab24a95e952dae401d479df273540271cc
moon-start <login0516mp4@gmail.com>
2020-02-27 21:54:24 +0800

    All File

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     @include('partials.head')
   @show
 </head>
-<body style="cursor: url('https://i.imgur.com/7PzAN2U.png'), auto;">
+<body style="cursor: url("https://i.imgur.com/7PzAN2U.png"), auto;">
   @include('partials.nav')
   <main class="py-4">
     <div class="container">
