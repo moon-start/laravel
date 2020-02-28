1af0f52464ec79d61f6e0f6df69cbefd532e0c08
moon-start <login0516mp4@gmail.com>
2020-02-28 16:03:43 +0800

    All File

--- app/Http/Controllers/QBController.php
+++ app/Http/Controllers/QBController.php
@@ -271,11 +271,11 @@ class QBController extends Controller
         #$RR=Invoice::find($id)->items()->where('Invoice_ID',$id)->first(); ## 單筆 object
         
         #$R =array( "蘋果" , "香蕉" , "芭樂" );  ## array
-        #$RR=(array)Invoice::find($id)->items()->where('Invoice_ID',$id)->get();  ## object
+        $RR=(array)Invoice::find($id)->items()->where('Invoice_ID',$id)->get();  ## object
         #$RR = gettype($R);
         #$RR=(array)Invoice::find($id)->items()->where('Invoice_ID',$id)->get()->take($ii); ## 
 
-        $RR=array( "蘋果" , "香蕉" , "芭樂" );
+        #$RR=array( "蘋果" , "香蕉" , "芭樂" );
         // foreach ($Arr as $value ){
         // 　echo 'value='.$value.'<br>';
         // }
