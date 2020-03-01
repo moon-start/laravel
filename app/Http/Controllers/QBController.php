e0f00973760c44508fa741025483d52e30e9fbbe
moon-start <login0516mp4@gmail.com>
2020-03-02 04:48:35 +0800

    All File

--- app/Http/Controllers/QBController.php
+++ app/Http/Controllers/QBController.php
@@ -150,7 +150,7 @@ class QBController extends Controller
         ## 傳入 make 方法的第一個參數是待驗證的資料，第二個參數是資料的驗證規則。
         $validator = Validator::make(
             $request->all(),[                  
-                'payment' => 'required|string|regex:(/^(\w{2}-)?(\d{8})?/u)',
+                'payment' => 'required|string|regex:(/^(\w{2}-)?(\d{8})?/u',
                 'points' => 'required|string'
             ],[
                 'required' => '必填欄位',
@@ -160,6 +160,8 @@ class QBController extends Controller
                 'regex' => '填入發票號碼'
             ]
         );
+        ## |regex:(/^(\w{2})?/-(\d{8})?/u',
+
         #(\d{3}-|\d{4}-)?(\d{8}|\d{7})?
         #(\w{2}-)?(\d{8})?
         #regex:/(^([a-zA-z]+)-(\d+)?$)/u   /^\d{4}-\d{2}-\d{2}$/',
