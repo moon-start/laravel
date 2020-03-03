334e2f60d29d1ded8438d1aa027f99191687e718
moon-start <login0516mp4@gmail.com>
2020-03-03 17:49:42 +0800

    All File

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -82,12 +82,12 @@
 
         <thead class="thead-darty">
             <tr class="row">
-            <th class="d-md-inline">日期</th>
-            <th class="d-md-inline">發票</th>
-            <th class="d-none d-md-inline">地點</th>
-            <th class="d-none d-md-inline">付款</th>
-            <th class="d-none d-md-inline">點數</th>
-            <td class="d-md-inline">資料(項目處理)</td>
+            <th class="col-md-2 d-md-inline">日期</th>
+            <th class="col-md-2 d-md-inline">發票</th>
+            <th class="col-md-2 d-none d-md-inline">地點</th>
+            <th class="col-md-2 d-none d-md-inline">付款</th>
+            <th class="col-md-2 d-none d-md-inline">點數</th>
+            <td class="col-md-2 d-md-inline">資料(項目處理)</td>
             </tr>
         </thead>
         <tbody>
@@ -101,15 +101,15 @@
                     <!-- 問號 d-md-inline -->
                     <!-- d-md-inline-block -->
                     <!-- d-md-inline -->
-                    <td class="d-md-inline"><?php echo $customer->paymentOf->date; ?></td>
-                    <td class="d-md-inline"><?php echo $customer->Invoice; ?></td>
-                    <td class="d-none d-md-inline"><?php echo $customer->location; ?></td>
-                    <td class="d-none d-md-inline"><?php echo $customer->paymentOf->payment; ?></td>
-                    <td class="d-none d-md-inline"><?php echo $customer->paymentOf->points; ?></td>
+                    <td class="col-md-2 d-md-inline"><?php echo $customer->paymentOf->date; ?></td>
+                    <td class="col-md-2 d-md-inline"><?php echo $customer->Invoice; ?></td>
+                    <td class="col-md-2 d-none d-md-inline"><?php echo $customer->location; ?></td>
+                    <td class="col-md-2 d-none d-md-inline"><?php echo $customer->paymentOf->payment; ?></td>
+                    <td class="col-md-2 d-none d-md-inline"><?php echo $customer->paymentOf->points; ?></td>
                
-                <td>
+                <td class="col-md-2 d-md-inline">
                   
-                    <a href="{{ action('QBController@newB', ['id'=>$customer->id]) }}" class="d-md-inline btn btn-success btn-sm">新增</a>
+                    <a href="{{ action('QBController@newB', ['id'=>$customer->id]) }}" class="btn btn-success btn-sm">新增</a>
 
                     <!-- <a href=" action('QBController@edit', 
                                 ['Cusid'=>$customer->Cusid,
