1701fbfdca74bbfab6e71c1f19e76938e6d7ea41
moon-start <login0516mp4@gmail.com>
2020-03-03 21:03:20 +0800

    All File

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -6,7 +6,7 @@
     <div class="card">
     <div class="card-header">消費列表</div>
     <div class="card-body p-1">
-        <table class="text-center table table-hover m-0  bg-info">
+        <table class="text-center table table-hover  bg-info">
         <!-- table-hover 移動效果 -->
         <!-- margin =0 -->
         <!-- bg-danger 背景顏色 -->
