993ffd76ddecc793a1f37c875b4110b3bbac2dd6
moon-start <login0516mp4@gmail.com>
2020-03-04 17:40:32 +0800

    All File

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -28,6 +28,10 @@
                                 新增消費
                                 </a>
                             </li>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                            </a>
                             
                     </ul>
 
@@ -68,7 +72,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="">
+                                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
