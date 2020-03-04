4e4bb9e9cec889ff36cb7d8b0ba489334a5d4586
moon-start <login0516mp4@gmail.com>
2020-03-04 17:29:08 +0800

    All File

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -72,11 +72,10 @@
                                 <!-- aria-labelledby="dropdown-toggle的ID" -->
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="OK">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                       onclick="">
+                                        {{ Auth::logout() }}
                                     </a>
-
+                                    
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
