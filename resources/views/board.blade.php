5c3c21f6b1d148ba00e2e80e1ce0342ad8bf0c09
moon-start <login0516mp4@gmail.com>
2020-03-04 16:45:45 +0800

    All File

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -121,7 +121,7 @@
                
                 <td class="col-sm-5 ol-md-5 col-lg-3 d-md-inline">
                     
-                    <a href="{{ action('QBController@newB', ['id'=>$customer->id]) }}" class="mx-5 btn btn-success btn-sm">新增</a>
+                    <a href="{{ action('QBController@newB', ['id'=>$customer->id]) }}" class="mx-sm-5 btn btn-success btn-sm">新增</a>
 
                     <!-- <a href=" action('QBController@edit', 
                                 ['Cusid'=>$customer->Cusid,
@@ -131,7 +131,7 @@
 
                     <a href="{{ action('QBController@edit',['id'=>$customer->id]) }} " class="btn btn-success btn-sm">編輯</a>
 
-                    <a href="{{ action('QBController@delete', ['id'=>$customer->id]) }}" class="mx-5 btn btn-danger btn-sm">刪除</a>
+                    <a href="{{ action('QBController@delete', ['id'=>$customer->id]) }}" class="mx-sm-5 btn btn-danger btn-sm">刪除</a>
                 </td>
                
 
