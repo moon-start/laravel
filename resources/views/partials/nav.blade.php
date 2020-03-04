648eecddae400a17b97a5784b762169f0f67b4b2
moon-start <login0516mp4@gmail.com>
2020-03-04 18:18:57 +0800

    All File

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -28,15 +28,7 @@
                                 新增消費
                                 </a>
                             </li>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="">
-                                        {{ __('Logout') }}
-                            </a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                            </a>
-                            
+                           
                     </ul>
 
 
@@ -70,16 +62,13 @@
                                 </div>
                             </div> -->
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
+                                
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="">
+                                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
