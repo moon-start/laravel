8ee889a8ad2fae3e3c3bba8548c7eb602b2f06a6
moon-start <login0516mp4@gmail.com>
2020-03-07 03:16:00 +0800

    All File

--- resources/views/newB.blade.php
+++ resources/views/newB.blade.php
@@ -59,11 +59,11 @@
             </div>
             
             <div class="form-group row">
-                <div class="col-lg-1"></div>
-                <div class="col-lg-9">
+                <div class="col-2"></div>
+                <div class="col-7">
                     <input type="submit" class="btn btn-primary" value="送出">
                 </div>
-                <div class="col-lg-1">
+                <div class="col-1">
                     <input type="submit" class="btn btn-warning" value="取消" name="cancel">
                 </div>
             </div>  
